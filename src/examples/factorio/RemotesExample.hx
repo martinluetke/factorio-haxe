23a593288dcc5a45dbef0e3f1cae4f148f35dfaf
mluetke <martin.luetke@googlemail.com>
2017-07-02 19:08:20 +0200

    improved remotes example

--- src/examples/factorio/RemotesExample.hx
+++ src/examples/factorio/RemotesExample.hx
@@ -4,35 +4,50 @@ import factorio.Game;
 import factorio.Script;
 import factorio.Remote;
 
+typedef MyInterface = {
+    var hello:String -> String;
+}
+
 class RemotesExample
 {
     var myName = "RemotesExample";
 
     public function new()
     {
-        Script.on_event(Events.on_player_created, player_created);
+        Script.on_init(this.register_interface);
+        Script.on_load(this.register_interface);
+        Script.on_event(Events.on_tick, this.test_interface);
     }
 
-    function player_created(event:PlayerCreatedEvent)
+    function register_interface()
     {
-        // var player = Game.players[event.player_index];
-        Game.print("Registering remote interface...");
+        // Dont list the functions of your interface in the curly brackets. 
+        // Haxe-lua will wrongly assume you are setting up a class method and add a hidden self parameter.
+        var interface1:Dynamic = {} 
+        // Always assign your functions like this. Then haxe-lua wont create a self parameter. Sadly this is a neccessary hack atm ...
+        interface1.hello = this.hello;
 
-        var interface1:Dynamic = {}
-        interface1.hello = hello;
+        var res = interface1.hello("Hi");
 
-        Remote.add_interface("remotes_example", interface1);
+        
+        Remote.add_interface("RemotesExample", interface1);
+    }
 
+    function test_interface(event:TickEvent)
+    {
         Game.print("Making a test call...");
 
-        var result:String = Remote.call("remotes_example","hello","Test Call");
+        var result:String = Remote.call("RemotesExample","hello","Test Call");
 
         Game.print("Received: " + result);
+
+        // only test once
+        Script.on_event(Events.on_tick, null);
     }
 
     function hello(name:String):String
     {
-        Game.print('Hello $name! $myName greets you.');
+        Game.print('Hello $name! ${this.myName} greets you.');
 
         return "Bye...";
     }

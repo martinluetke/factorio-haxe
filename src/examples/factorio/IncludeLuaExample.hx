15eff58cdb4b7fc6ca1ecf759a0b134a9a8e7cfa
mluetke <martin.luetke@googlemail.com>
2017-07-06 03:24:23 +0200

    better IncludeLuaExample docu

--- src/examples/factorio/IncludeLuaExample.hx
+++ src/examples/factorio/IncludeLuaExample.hx
@@ -13,9 +13,9 @@ import factorio.Commands;
     Method Nr.1 - using `@:luaRequire`
 
     This will import any lua file and bind what it returns to a static class.
-    This only makes sense if that lua file returns a table with named data and or function fields.
+    This only makes sense if that lua file returns a table with named data and/or function fields.
 
-    Path is relative to the generated file, here that should be `control.lua`.
+    Path is relative to the generated file, f.i. `control.lua`.
     Don't add the '.lua' extension! It is implicitly added by lua's require(xyz) call.
 **/
 @:luaRequire("src/examples/factorio/IncludeLuaExample1")
@@ -25,7 +25,7 @@ extern class IncludeLuaExampleLua {
 }
 
 /** 
-    IncludeLuaExample - How to use an existing lua file or library
+    IncludeLuaExample - How to use existing lua code
 **/
 class IncludeLuaExample
 {
@@ -41,11 +41,11 @@ class IncludeLuaExample
 
     public static function __init__()
     {
-        // Method Nr.2
+        // Method Nr.2 - Using macro
         // Import any lua file at compile time. It will be inlined at the beginning of `control.lua`.
         // This is useful if your lua file is not organized like a module and creates global variables.
         // Please note that its code is added and therefore executed at the very beginning of control.lua, 
-        // not at the position of the includeFile macro!
+        // not at the position of the `Compiler.includeFile` macro!
 
         haxe.macro.Compiler.includeFile("src/examples/factorio/IncludeLuaExample2.lua");
     }
@@ -71,18 +71,20 @@ class IncludeLuaExample
         Game.print('[IncludeLuaExample] Method Nr.2 - globalVariable\'s value is $value and globalFunction() returned $ret');
 
 
-        // Method Nr.3
-        // Call arbitrary lua code
+        // Method Nr.3 - Call arbitrary lua code via __lua__("...")
+        // This useful if you need to hack around haxe bugs and limitations, 
+        // or to simply copy and paste some lua code.
 
-        var ret:String = untyped __lua__("(function(param) 
-            return \"foo\" .. param 
-        end)(\"bar\")
+        var ret:String = untyped __lua__("
+            (function(param) 
+                return \"foo\" .. param 
+            end)(\"bar\")
         ");
 
         Game.print("[IncludeLuaExample] Method Nr.3 - It returned " + ret);
 
         
-        // only test once
+        // only tick once
         Script.on_event(Events.on_tick, null);
     }
 }
